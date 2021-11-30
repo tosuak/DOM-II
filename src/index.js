8f36d88971bec9b3784931ae74ba4b6aed352985
Tafiqul <demianviotto@gmail.com>
2021-11-30 18:26:57 -0500

    working on project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './less/index.less'
 
 // Your code goes here!
+const topHeader = document.querySelector('.intro h2');
+const topImg = document.querySelectorAll('button');
+
+const mouseOver = (evt) => {
+    evt.target.style.color = 'red';
+}
+
+topHeader.addEventListener('mouseover', mouseOver);
+
+const doubleClick = (evt) => {
+    evt.target.style.color = 'green';
+};
+
+topImg.addEventListener('mouseover', doubleClick);
