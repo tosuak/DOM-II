02d4dde316207bbe9d63aea7cc8fcf2ceb1c8bf7
Tafiqul <demianviotto@gmail.com>
2021-11-30 20:18:21 -0500

    finished project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import './less/index.less'
 // Your code goes here!
 const topHeader = document.querySelector('.intro h2');
 const Img = document.querySelector('.intro img');
-const midImg = document.querySelector('.img-content img')
-
+const midImg1 = document.querySelector('.img-content img')
+const midImg2 = midImg1.nextElementSibling;
 
 const mouseOver = (evt) => {
     evt.target.style.color = 'red';
@@ -30,10 +30,23 @@ const zoom = () => {
    document.body.style.scale = 2;
 }
 
-midImg.addEventListener('click', zoom);
+midImg2.addEventListener('click', zoom);
 
-// const focus = () => {
-//     document. = '';
-// }
-
-// midImg.addEventListener('mouseover', blur)
+const animateImg = () => {
+midImg1.animate([
+    { transform: 'translateY(0px)' },
+    { transform: 'translateY(-300px)' }
+  ], {
+    duration: 1000,
+    iterations: Infinity
+  })
+  midImg2.animate([
+    { transform: 'translateY(300px)' },
+    { transform: 'translateY(0px)' }
+  ], {
+    duration: 1000,
+    iterations: Infinity
+  })
+}
+midImg1.addEventListener('click', animateImg);
+midImg2.addEventListener('click', animateImg);
